5c684d39fd6844f842091962ea763c529241763c
Marcin <xcinek1337@wp.pl>
2023-11-06 10:37:05 +0100

    refactored forms

--- src/components/CalendarForm.js
+++ src/components/CalendarForm.js
@@ -1,110 +1,18 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
 
-import { saveMeetingAction } from './actions/calendar';
-import { saveMeetingInApi } from './providers/meetingsApi';
-import validateForm from './providers/validateForm';
-
+import CalendarFormHandler from './CalendarFormHandler';
 
 const CalendarForm = () => {
-	const dispatch = useDispatch();
+	 const fields= [ 
+		{ name: 'firstName', type: 'text', placeHolder: '', validation: { isReq: true } },
+		{ name: 'lastName', type: 'text', placeHolder: '', validation: {  isReq: true } },
+		{ name: 'email', type: 'text', placeHolder: 'nazwa@poczty.pl', validation: { regex: /^[0-9a-zA-Z_.-]+@[0-9a-zA-Z.-]+\.[a-zA-Z]{2,3}$/ , isReq: true } },
+		{ name: 'date', type: 'text', placeHolder: 'RRRR-MM-DD', validation: { regex: /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/ , isReq: true } },
+		{ name: 'time', type: 'text', placeHolder: 'HH:MM', validation: { regex: /^[0-9]{2}:[0-9]{2}$/ , isReq: true } },	
+	]
 
-	const [state, setState] = useState({
-		firstName: '',
-		lastName: '',
-		email: '',
-		date: '',
-		time: '',
-		id: '',
-		errors: [],
-	});
-
-	const isFieldNameCorrect = name => {
-		const fieldsData = getFieldsData();
-		console.log(typeof fieldsData[name] !== 'undefined');
-		return typeof fieldsData[name] !== 'undefined';
-	};
-
-	const handleFieldChange = e => {
-		if (isFieldNameCorrect(e.target.name)) {
-			setState({ ...state, [e.target.name]: e.target.value, id: uuidv4() });
-		}
-	};
-	
-	const getFieldsData = () => {
-		const fieldsData = { ...state };
-		delete fieldsData['errors'];
-		return fieldsData;
-	};
-
-	const handleSubmit = e => {
-		e.preventDefault();
-
-		const errors = validateForm(state);
-		setState({ ...state, errors });
-
-		if (errors.length === 0) {
-			saveMeeting();
-			clearFormFields();
-		}
-	};
-
-
-	const saveMeeting = () => {
-		dispatch(saveMeetingAction(getFieldsData()));
-
-		saveMeetingInApi(getFieldsData());
-	};
-
-	return (
-		<form onSubmit={handleSubmit}>
-			<ul>{renderErrors()}</ul>
-			<div>
-				<label>
-					Data: <input name='date' onChange={handleFieldChange} value={state.date} placeholder='RRRR-MM-DD' />
-				</label>
-			</div>
-			<div>
-				<label>
-					Godzina: <input name='time' onChange={handleFieldChange} value={state.time} placeholder='HH:MM' />
-				</label>
-			</div>
-			<div>
-				<label>
-					Imię: <input name='firstName' onChange={handleFieldChange} value={state.firstName} />
-				</label>
-			</div>
-			<div>
-				<label>
-					Nazwisko: <input name='lastName' onChange={handleFieldChange} value={state.lastName} />
-				</label>
-			</div>
-			<div>
-				<label>
-					Email: <input name='email' onChange={handleFieldChange} value={state.email} placeholder='nazwa@poczty.pl' />
-				</label>
-			</div>
-			<div>
-				<input type='submit' value='Zapisz' />
-			</div>
-		</form>
-	);
-
-	function renderErrors() {
-		if (state.errors) {
-			return state.errors.map((err, index) => <li key={index}>{err}</li>);
-		}
-	}
-
-	function clearFormFields() {
-		const fieldsData = getFieldsData();
-		for (const prop in fieldsData) {
-			fieldsData[prop] = '';
-		}
-
-		setState(fieldsData);
-	}
+	return <CalendarFormHandler fields={fields} />
 };
 
 export default CalendarForm;
+
---
+++ src/components/CalendarFormHandler.js
@@ -0,0 +1,98 @@
+import React, { useReducer, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+
+import { saveMeetingAction } from './actions/calendar';
+import { saveMeetingInApi } from './providers/meetingsApi';
+
+const CalendarFormHandler = ({ fields }) => {
+	const init = { date: '', email: '', firstName: '', lastName: '', time: '' };
+
+	const reduxDispatch = useDispatch();
+	const [state, dispatch] = useReducer(reducer, init);
+	const [errors, setErrors] = useState([]);
+
+	function reducer(state, action) {
+		switch (action.type) {
+			case 'change': {
+				return { ...state, [action.key]: action.value };
+			}
+			case 'init': {
+				return { ...state, ...action.data };
+			}
+			default:
+				return state;
+		}
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const validationErrors = validate(fields, state);
+		if (validationErrors.length === 0) {
+			const newMeetingWithId = { ...state, id: uuidv4() };
+			reduxDispatch(saveMeetingAction(newMeetingWithId));
+			saveMeetingInApi(newMeetingWithId);
+
+			dispatch({ type: 'init', data: init });
+		}
+	}
+	function validate(state, values) {
+		const errors = [];
+
+		state.forEach(({ name, validation }) => {
+			const value = values[name];
+			if (validation.isReq && value.trim() === '') {
+				errors.push(`pole ${name} jest wymagane.`);
+			}
+			if (validation.regex && !validation.regex.test(value)) {
+				errors.push(`pole ${name} jest wypelnione nieprawidlowo.`);
+			}
+		});
+		setErrors(errors);
+		return errors;
+	}
+	function renderErros() {
+		return (
+			<ul className='form__error-list'>
+				{errors.map((message, index) => (
+					<li className='form__error-item' key={index}>
+						{message}
+					</li>
+				))}
+			</ul>
+		);
+	}
+
+	function renderFieldList() {
+		return fields.map(({ name, type, placeHolder }) => {
+			const value = state[name];
+
+			return (
+				<div key={name || type}>
+					<label className='form__label' htmlFor={name}>
+						{name}
+						<input
+							placeholder={placeHolder}
+							onChange={e => dispatch({ type: 'change', key: name, value: e.target.value })}
+							type={type}
+							name={name}
+							id={name}
+							value={value}
+						/>
+					</label>
+				</div>
+			);
+		});
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			{renderErros()}
+			{renderFieldList()}
+			<input type='submit' value='hejka tu lenka' />
+		</form>
+	);
+};
+
+export default CalendarFormHandler;
